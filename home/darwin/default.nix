afe13fc812a05614c9382e030e2efc0bb9a7c3c3
adsellor <22825451+adsellor@users.noreply.github.com>
2025-04-02 19:10:20 +0400

    add fish shortcuts

--- home/darwin/default.nix
+++ home/darwin/default.nix
@@ -1,4 +1,4 @@
-{ username, lib, inputs, ... }:
+{ username, lib, inputs, host, ... }:
 {
   imports = [
     ../base/default.nix
@@ -8,6 +8,14 @@
   home.homeDirectory = lib.mkForce "/Users/${username}";
 
   programs.fish.shellAliases = {
-    fr = "darwin-rebuild switch --flake /Users/${username}/zaneyos";
+    fr = "darwin-rebuild switch --flake /Users/${username}/Developer/ads-zaneyos .#${host}";
+    cfig = "cd /Users/${username}/Developer/ads-zaneyos && nvim .";
+    fu = "darwin-rubild switch --hostname ${host} --update /Users/${username}/Developer/ads-zaneyos";
+    ncg = "nix-collect-garbage --delete-old && sudo nix-collect-garbage -d";
+    cat = "bat";
+    ls = "eza --icons";
+    ll = "eza -lh --icons --grid --group-directories-first";
+    la = "eza -lah --icons --grid --group-directories-first";
+    ".." = "cd ..";
   };
 }
